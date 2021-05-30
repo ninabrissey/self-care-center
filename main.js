0822e16a6af914a7ac1953a0fa82e764102b8059
Nina Brissey <nbrissey@gmail.com>
2021-05-30 13:29:32 -0700

    Refactor receiveMessage function logic, rename variables, organize page

--- main.js
+++ main.js
@@ -6,72 +6,7 @@ var bell = document.querySelector('#meditation-bell-icon');
 var message = document.querySelector('#motivational-message-display');
 var buttonSelectAnotherMessage = document.querySelector('#select-another-message-button');
 var radioButtons = document.querySelector('#radio-buttons');
-
-
-// event-listeners 👇
-buttonReceiveMessage.addEventListener('click', receiveMessage);
-buttonSelectAnotherMessage.addEventListener('click', selectAnotherMessage);
-selectAffirmation.addEventListener('change', revealReceiveMessageButton);
-selectMantra.addEventListener('change', revealReceiveMessageButton);
-buttonReceiveMessage.addEventListener('click', receiveMessage);
-
-
-
-// functions and event handlers 👇
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-};
-
-function receiveMessage() {
-  if (selectMantra.checked) {
-    message.innerHTML = mantras[getRandomIndex(mantras)];
-  } else if (selectAffirmation.checked) {
-    message.innerHTML = affirmations[getRandomIndex(affirmations)];
-  } else {
-    message.innerHTML = 'Please select a mantra or affirmation.';
-  }
-  currentMessage = message.innerHTML;
-  hideMeditationBell();
-  revealSelectAnotherMessageButton();
-};
-
-function revealReceiveMessageButton() {
-  if (selectAffirmation.checked || selectMantra.checked) {
-    show(buttonReceiveMessage);
-  }
-};
-
-function hideMeditationBell() {
-  hide(bell);
-  show(message);
-};
-
-function revealSelectAnotherMessageButton() {
-  hide(buttonReceiveMessage);
-  hide(radioButtons);
-  show(buttonSelectAnotherMessage);
-};
-
-function selectAnotherMessage() {
-  hide(message);
-  hide(buttonSelectAnotherMessage);
-  show(radioButtons);
-
-  show(bell);
-  show(buttonReceiveMessage);
-};
-
-function hide(element) {
-  element.classList.add('hidden');
-};
-
-function show(element) {
-  element.classList.remove('hidden');
-};
-
-// data model 👇
 var currentMessage = 'Please select mantra or affirmation to receive a message';
-
 var affirmations = [
   'I forgive myself and set myself free.',
   'I believe I can be all that I want to be.',
@@ -87,7 +22,6 @@ var affirmations = [
   'I honor my body by trusting the signals that it sends me.',
   'I manifest perfect health by making smart choices.'
 ];
-
 var mantras = [
   'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
   'Don’t let yesterday take up too much of today.',
@@ -105,3 +39,60 @@ var mantras = [
   'Onward and upward.',
   'I am the sky, the rest is weather.'
 ];
+
+// event-listeners 👇
+buttonReceiveMessage.addEventListener('click', receiveMessage);
+buttonSelectAnotherMessage.addEventListener('click', selectAnotherMessage);
+selectAffirmation.addEventListener('change', showReceiveMessageButton);
+selectMantra.addEventListener('change', showReceiveMessageButton);
+buttonReceiveMessage.addEventListener('click', receiveMessage);
+
+// functions and event handlers 👇
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+function receiveMessage() {
+  if (selectMantra.checked) {
+    message.innerHTML = mantras[getRandomIndex(mantras)];
+  }
+  if (selectAffirmation.checked) {
+    message.innerHTML = affirmations[getRandomIndex(affirmations)];
+  }
+  currentMessage = message.innerHTML;
+  hideBell();
+  showNewMessage();
+};
+
+function showReceiveMessageButton() {
+  if (selectAffirmation.checked || selectMantra.checked) {
+    show(buttonReceiveMessage);
+  }
+};
+
+function hideBell() {
+  hide(bell);
+  show(message);
+};
+
+function showNewMessage() {
+  hide(buttonReceiveMessage);
+  hide(radioButtons);
+  show(buttonSelectAnotherMessage);
+};
+
+function selectAnotherMessage() {
+  hide(message);
+  hide(buttonSelectAnotherMessage);
+  show(radioButtons);
+  show(bell);
+  show(buttonReceiveMessage);
+};
+
+function hide(element) {
+  element.classList.add('hidden');
+};
+
+function show(element) {
+  element.classList.remove('hidden');
+};
