837cfb8b439d4c04b29c6eebef028b096c37296c
Nina Brissey <nbrissey@gmail.com>
2021-05-30 06:03:52 -0700

    Fix bug on selectAnotherMessage function and clean up semi-colons/spacing

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var selectAffirmation = document.querySelector('#affirmation-button');
 var motivationalMessageBell = document.querySelector('#meditation-bell-icon');
 var motivationalMessage = document.querySelector('#motivational-message-display');
 var buttonSelectAnotherMessage = document.querySelector('#select-another-message-button');
-
+var radioButtons = document.querySelector('#radio-buttons');
 
 
 // event-listeners 👇
@@ -20,14 +20,8 @@ buttonReceiveMessage.addEventListener('click', receiveMessage);
 // functions and event handlers 👇
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
 
-function hideMeditationBell() {
-  hide(motivationalMessageBell);
-  show(motivationalMessage);
-}
-
-// for function below, I would like to make it dynamic, but I done understand how to the computer would know which arguments to pass in. I think we could remove two lines of code if this were the case. 👇
 function receiveMessage() {
   if (selectMantra.checked) {
     motivationalMessage.innerHTML = mantras[getRandomIndex(mantras)];
@@ -39,37 +33,43 @@ function receiveMessage() {
   currentMessage = motivationalMessage.innerHTML;
   hideMeditationBell();
   revealSelectAnotherMessageButton();
-}
+};
 
 function revealReceiveMessageButton() {
   if (selectAffirmation.checked || selectMantra.checked) {
     show(buttonReceiveMessage);
   }
-}
+};
+
+function hideMeditationBell() {
+  hide(motivationalMessageBell);
+  show(motivationalMessage);
+};
 
 function revealSelectAnotherMessageButton() {
   hide(buttonReceiveMessage);
+  hide(radioButtons);
   show(buttonSelectAnotherMessage);
-}
+};
 
 function selectAnotherMessage() {
   hide(motivationalMessage);
   hide(buttonSelectAnotherMessage);
+  show(radioButtons);
 
   show(motivationalMessageBell);
   show(buttonReceiveMessage);
-}
+};
 
 function hide(element) {
   element.classList.add('hidden');
-}
+};
 
 function show(element) {
   element.classList.remove('hidden');
-}
+};
 
-
-
+// data model 👇
 var currentMessage = 'Please select mantra or affirmation to receive a message';
 
 var affirmations = [
@@ -86,7 +86,7 @@ var affirmations = [
   'Every day I am getting healthier and stronger.',
   'I honor my body by trusting the signals that it sends me.',
   'I manifest perfect health by making smart choices.'
-]
+];
 
 var mantras = [
   'Breathing in, I send myself love. Breathing out, I send love to someone else who needs it.',
@@ -104,4 +104,4 @@ var mantras = [
   'The only constant is change.',
   'Onward and upward.',
   'I am the sky, the rest is weather.'
-]
+];
